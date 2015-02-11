e59462f7fcd8e0a118b6d2d83c2271564b489ef2
danBrunton <1bruntondan@hdsb.ca>
2015-02-10 20:21:38 -0500

    up to date code v 100

--- Wildebots2015/src/org/usfirst/frc/team4902/robot/Robot.java
+++ Wildebots2015/src/org/usfirst/frc/team4902/robot/Robot.java
@@ -13,6 +13,7 @@ import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.Gyro;
+import java.util.*;
 
 public class Robot extends IterativeRobot {
 	
@@ -28,7 +29,7 @@ public class Robot extends IterativeRobot {
         RobotDrive myRobot;
         Encoder liftEncoder, wheel1Encoder, wheel2Encoder, wheel3Encoder, wheel4Encoder;
         Gyro gyro;
-        PIDController pidController1, pidController2, pidController3, pidController4;
+        PIDController pidController;
         PIDOutput pidOutput1, pidOutput2, pidOutput3, pidOutput4;
         
         final int liftChannelA = 5;
@@ -63,41 +64,34 @@ public class Robot extends IterativeRobot {
         final double reverse = -1.0;
         final double delay = 1;
         
-        private boolean isAngleZero1 = true;
-        private double  PIDTime1 = 0;
-        private double PIDSpeed1 = 0;
-        private double changeAngle1 = 0;
-        private boolean isAngleZero2 = true;
-        private double  PIDTime2 = 0;
-        private double PIDSpeed2 = 0;
-        private double changeAngle2 = 0;
-        private boolean isAngleZero3 = true;
-        private double  PIDTime3 = 0;
-        private double PIDSpeed3 = 0;
-        private double changeAngle3 = 0;
-        private boolean isAngleZero4 = true;
-        private double  PIDTime4 = 0;
-        private double PIDSpeed4 = 0;
-        private double changeAngle4 = 0;
+        private PIDOutput pidOutput;       
         
+    public class PrintPIDOutput implements PIDOutput{    
+    	
+    	 public void pidWrite(double output){
+    		 System.out.println(pidOutput);
+    	 }
+    	
+    }
         
     public void robotInit() {
        
         
+    	
         frontLeftMotor = new Talon(frontLeftChannel);
         frontRightMotor = new Talon(frontRightChannel);
         rearLeftMotor = new Talon(rearLeftChannel);
         rearRightMotor = new Talon(rearRightChannel);
         
         liftMotor = new Talon(liftChannel);
-        s1.set(true);
+        //s1.set(true);
         
         myRobot = new RobotDrive(frontLeftMotor, rearLeftMotor, frontRightMotor, rearRightMotor);
         
-        myRobot.setInvertedMotor(MotorType.kFrontRight, true);
-        myRobot.setInvertedMotor(MotorType.kFrontLeft, false);
-        myRobot.setInvertedMotor(MotorType.kRearRight, true);
-        myRobot.setInvertedMotor(MotorType.kRearLeft, false);
+        myRobot.setInvertedMotor(MotorType.kFrontRight, false);
+        myRobot.setInvertedMotor(MotorType.kFrontLeft, true);
+        myRobot.setInvertedMotor(MotorType.kRearRight, false);
+        myRobot.setInvertedMotor(MotorType.kRearLeft, true);
         
         stick = new Joystick(joystickChannel);
         
@@ -109,13 +103,6 @@ public class Robot extends IterativeRobot {
     	gyro.initGyro();
     	gyro.reset();
         
-//        liftEncoder = new Encoder(2, 3, true, EncodingType.k4X);
-//        liftEncoder.setMaxPeriod(.1);
-//        liftEncoder.setMinRate(10);
-//        liftEncoder.setDistancePerPulse(5);
-//        liftEncoder.setReverseDirection(true);
-//        liftEncoder.setSamplesToAverage(7);
-        
         wheel1Encoder = new Encoder(1, 2, true, EncodingType.k4X);
         wheel1Encoder.setMaxPeriod(.1);
         wheel1Encoder.setMinRate(10);
@@ -123,36 +110,7 @@ public class Robot extends IterativeRobot {
         wheel1Encoder.setReverseDirection(true);
         wheel1Encoder.setSamplesToAverage(7);
         
-//        wheel2Encoder = new Encoder(2, 3, true, EncodingType.k4X);
-//        wheel2Encoder.setMaxPeriod(.1);
-//        wheel2Encoder.setMinRate(10);
-//        wheel2Encoder.setDistancePerPulse(5);
-//        wheel2Encoder.setReverseDirection(true);
-//        wheel2Encoder.setSamplesToAverage(7);
-        
-//        wheel3Encoder = new Encoder(2, 3, true, EncodingType.k4X);
-//        wheel3Encoder.setMaxPeriod(.1);
-//        wheel3Encoder.setMinRate(10);
-//        wheel3Encoder.setDistancePerPulse(5);
-//        wheel3Encoder.setReverseDirection(true);
-//        wheel3Encoder.setSamplesToAverage(7);
-        
-//        wheel4Encoder = new Encoder(2, 3, true, EncodingType.k4X);
-//        wheel4Encoder.setMaxPeriod(.1);
-//        wheel4Encoder.setMinRate(10);
-//        wheel4Encoder.setDistancePerPulse(5);
-//        wheel4Encoder.setReverseDirection(true);
-//        wheel4Encoder.setSamplesToAverage(7);
-        
-        //p is the gyro angle
-        //i is the time away from 0
-        //d is the change in angle over a set time
-        
-        pidController1 = new PIDController(gyro.getAngle(), 0, 0, gyro, frontRightMotor);
-        pidController2 = new PIDController(gyro.getAngle(), 0, 0, gyro, frontLeftMotor);
-        pidController3 = new PIDController(gyro.getAngle(), 0, 0, gyro, rearRightMotor);
-        pidController4 = new PIDController(gyro.getAngle(), 0, 0, gyro, rearLeftMotor);
-        
+
         
     }
 
@@ -175,34 +133,41 @@ public class Robot extends IterativeRobot {
     	return input;
     }
     
+    public void teleopInit(){
+    	
+    	System.out.println("pidOutput create");
+        pidOutput = new PrintPIDOutput();
+        System.out.println("made it through pidOutput");
+        pidController = new PIDController(gyro.getAngle(), 0, 0, gyro, pidOutput);
+        System.out.println("created the pid Controller");
+        pidController.enable();
+    }
+    
     public void teleopPeriodic() {
         
-        System.out.println(stick.getX() + ", " + stick.getY());
+        //System.out.println(stick.getX() + ", " + stick.getY());
+        double inputX = joystickZeroed(stick.getX());
+        double inputY = joystickZeroed(stick.getY());
+        double inputZ = joystickZeroed(stick.getZ());
+        
+        myRobot.mecanumDrive_Cartesian(-inputX, -inputY, -0.35*inputZ, 0);
+                
+        Timer.delay(0.005);
+    }
+    
+    public void testPeriodic() {     
+    	
         double inputX = joystickZeroed(stick.getX());
         double inputY = joystickZeroed(stick.getY());
         double inputZ = joystickZeroed(stick.getZ());
         
         myRobot.mecanumDrive_Cartesian(-inputX, -inputY, -0.35*inputZ, 0);
         
-//        double x = stick.getX();
-//        
-//        frontLeftMotor.set(x);
-//        frontRightMotor.set(x);
-//        rearLeftMotor.set(x);
-//        rearRightMotor.set(x);
-
-                
         Timer.delay(0.005);
-    }
-    
-    public void testPeriodic() {        
         
         //test3();
-    	test4();
     	//test2();
-    	//lift();
-    	//PID();
-        
+    	//lift();        
     }
     
     public void test1(){
@@ -288,11 +253,6 @@ public class Robot extends IterativeRobot {
     	
     }
     
-    public void test4() {
-    	System.out.println(gyro.getAngle());
-
-    }
-    
     public void lift() {
     	boolean applyBrake = true;
     	boolean liftUp = stick.getRawButton(1);
@@ -328,61 +288,9 @@ public class Robot extends IterativeRobot {
     	}
     }
     
-    public void PID(){
+    public void pidCorrection(){
     	
-        //p is the gyro angle
-        //i is the time away from 0
-        //d is the change in angle over a set time
-    	
-    	if(gyro.getAngle() != 0){
-    		if((isAngleZero1 == true)&&(gyro.getAngle() != 0)){
-    			PIDTime1 = 0;
-    			isAngleZero1 = false;
-    			changeAngle1 = gyro.getAngle();
-    		}
-    		if((isAngleZero2 == true)&&(gyro.getAngle() != 0)){
-    			PIDTime2 = 0;
-    			isAngleZero2 = false;
-    			changeAngle2 = gyro.getAngle();
-    		}
-    		if((isAngleZero3 == true)&&(gyro.getAngle() != 0)){
-    			PIDTime3 = 0;
-    			isAngleZero3 = false;
-    			changeAngle3 = gyro.getAngle();
-    		}
-    		if((isAngleZero4 == true)&&(gyro.getAngle() != 0)){
-    			PIDTime4 = 0;
-    			isAngleZero4 = false;
-    			changeAngle4 = gyro.getAngle();
-    		}
-    		
-    		
-    		PIDSpeed1 = (changeAngle1-gyro.getAngle())/PIDTime1;
-    		PIDSpeed2 = (changeAngle2-gyro.getAngle())/PIDTime2;
-    		PIDSpeed3 = (changeAngle3-gyro.getAngle())/PIDTime3;
-    		PIDSpeed4 = (changeAngle4-gyro.getAngle())/PIDTime4;
-    		
-        	// pidController = new PIDController(gyro.getAngle(), i, d, gyro, frontLeftMotor);
-        	pidController1.setPID(gyro.getAngle(), PIDTime1, PIDSpeed1);
-        	pidController2.setPID(gyro.getAngle(), PIDTime2, PIDSpeed2);
-        	pidController3.setPID(gyro.getAngle(), PIDTime3, PIDSpeed3);
-        	pidController4.setPID(gyro.getAngle(), PIDTime4, PIDSpeed4);
-        	System.out.println(gyro.getAngle());
-        	System.out.println(pidController1.get() + ", " + pidController2.get() + ", " + pidController3.get() + ", " + pidController4.get());
-    		PIDTime1++;
-    		PIDTime2++;
-    		PIDTime3++;
-    		PIDTime4++;
-    		
-    		if(gyro.getAngle() == 0){
-    			isAngleZero1 = true;
-    			isAngleZero2 = true;
-    			isAngleZero3 = true;
-    			isAngleZero4 = true;
-    		}
-    	}
-
-       
-    }
+    	//pidController.setPID(temp, PIDTime1, PIDSpeed1);
     
+    }
 }
